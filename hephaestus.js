1cafd773085707e344ae884a2fbb383aa8eba786
Alberto Ortiz Flores <dismorfo@gmail.com>
2018-11-28 22:32:21 -0500

    update

--- hephaestus.js
+++ hephaestus.js
@@ -1,15 +1,12 @@
 'use strict'
 
 const agartha = process.agartha
-const appUrl = agartha.get('EXAMPLE_APP_URL')
-const appRoot = agartha.get('EXAMPLE_APP_ROOT')
+const appUrl = agartha.get('EXAMPLE_APP_URL');
 
 module.exports = exports = {
   appName: 'Hephaestus bare site',
   shortName: 'hephaestus-site-bare',
   appUrl: (appUrl) ? appUrl : 'http://127.0.0.1:8080',
-  appRoot:  (appRoot) ? appRoot : 'http://127.0.0.1:8080',
-  relic: 'scaffold',
   version: '0.0.1',
   hephaestus: '2.0.0'
 }
