8bca8a46b989bfe5441abcc775bf006604056215
Alberto Ortiz Flores <dismorfo@gmail.com>
2018-11-28 23:00:26 -0500

    update

--- app/commands/hephaestus-command-example/index.js
+++ app/commands/hephaestus-command-example/index.js
@@ -2,38 +2,18 @@
 
 const { agartha } = require('hephaestus');
 
-const HephaestusCommandExample = class {
+module.exports = exports = class HephaestusCommandExample extends agartha.Command {
 
   get command () {
     return 'hephaestus-command-example';
   }
 
-  get alias () {
-    return false;
-  }
-
   get description () {
-    return 'Simple command example';
-  }
-
-  get options () {
-    return [];
-  }
-
-  get onInit () {
-    return false;
-  }
-
-  get onDone () {
-    return false;
+    return 'Echo this command description';
   }
 
   action () {
-    
     agartha.log(this.description(), 'ok');
-
   }
 
 }
-
-module.exports = exports = HephaestusCommandExample;
