16c6a6796376fdd30f8d1ab822d91bdfb25fb9ff
Alberto Ortiz Flores <aortiz@nyu.edu>
2018-11-30 16:54:55 -0500

    Update index.js

--- app/pages/front/index.js
+++ app/pages/front/index.js
@@ -4,17 +4,7 @@ module.exports = exports = {
   id: 'home',
   title: 'Home',
   route: '/index.html',
-  menu: [
-    {
-      context: 'navbar',
-      label: 'Home',
-      weight: 1
-    }
-  ],
   content : {
     localsource: 'content.main.html'
-  },
-  assets: {
-    js: []
   }
 }
