d8323ce1b8585b972b54f45babdc5e100e279728
Alberto Ortiz Flores <aortiz@nyu.edu>
2018-11-30 22:10:40 -0500

    Update index.js

--- app/pages/front/index.js
+++ app/pages/front/index.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
-module.exports = exports = {
+module.exports = {
   id: 'home',
   title: 'Home',
   route: '/index.html',
   content : {
     localsource: 'content.main.html'
   }
-}
+};
